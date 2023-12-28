d4b4ee73500e195490abc14d8c19a2933add8eb8
Wim ten Brink <wim.tenbrink@wtss.nl>
2023-12-28 17:44:48 +0100

    First setup.

---
+++ LotteryApp/Helpers.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace LotteryApp;
+
+public class Pattern
+{
+    public long Value { get; set; } = 0;
+
+    public int BitCount
+    {
+        get
+        {
+            int result = 0;
+            long value = Value;
+            while (value > 0)
+            {
+                if ((value & 1) == 1) { result++; }
+                value >>= 1;
+            }
+            return result;
+        }
+    }
+
+    public long Add(int number)
+    {
+        Value |= 1L << number;
+        return Value;
+    }
+
+    public long Remove(int number)
+    {
+        Value &= ~(1L << number);
+        return Value;
+    }
+
+    public DrawNumbers Draw
+    {
+        get
+        {
+            var result = new DrawNumbers();
+            for (int i = 0; i < 47; i++)
+            {
+                if ((Value & (1L << i)) != 0) { result.Add(i); }
+            }
+            return result;
+        }
+    }
+
+    public override string ToString() { return Value.ToString("b64"); }
+}
+
+public class DrawNumbers : List<int>
+{
+    public Pattern Pattern
+    {
+        get
+        {
+            Pattern result = new Pattern();
+            foreach (var i in this) { result.Add(i); }
+            return result;
+        }
+    }
+
+    public override string ToString() { return string.Join(", ", this.Select(i => i.ToString())); }
+}
+
+public static class Helpers
+{
+    private static readonly Random Random = new();
+
+    public static DrawNumbers Draw(int count, int min, int max)
+    {
+        List<int> range = Enumerable.Range(min, max).ToList();
+        DrawNumbers drawNumbers = new DrawNumbers();
+        while (count-- > 0)
+        {
+            var index = Random.Next(range.Count);
+            drawNumbers.Add(range[index]);
+            range.RemoveAt(index);
+        }
+        return drawNumbers;
+    }
+
+    public static Pattern Combine(this Pattern pattern, Pattern other)
+    {
+        var result = new Pattern() { Value = pattern.Value };
+        result.Value &= other.Value;
+        return result;
+    }
+
+    public static bool Match(this Pattern pattern, Pattern other)
+    {
+        return (pattern.Value & other.Value) == pattern.Value;
+    }
+
+}
--- LotteryApp/Program.cs
+++ LotteryApp/Program.cs
@@ -1,2 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using LotteryApp;
+
+List<DrawNumbers> draws = Enumerable.Range(0, 600).ToList().Select(i => Helpers.Draw(9, 1, 47)).ToList();
+foreach (DrawNumbers draw in draws)
+{
+    Console.WriteLine($"{draw.Pattern} - {draw.Pattern.BitCount} - {draw.Pattern.Draw}");
+}
+
+List<Pattern> drawPatterns = draws.Select(d => d.Pattern).ToList();
+
+List<Pattern> patterns = new();
+for (int x = 0; x < drawPatterns.Count - 1; x++)
+{
+    for (int y = x + 1; y < drawPatterns.Count; y++)
+    {
+        patterns.Add(drawPatterns[x].Combine(drawPatterns[y]));
+    }
+}
+
+Console.WriteLine($"We now have {patterns.Count} patterns.");
+patterns = patterns.Select(p => p.Value).Distinct().Select(i => new Pattern() { Value = i }).ToList();
+Console.WriteLine($"We reduced this to {patterns.Count} patterns.");
+
+var occurances = patterns.Select(p => new KeyValuePair<Pattern, List<Pattern>>(p, drawPatterns.Where(r => p.Match(r)).ToList())).GroupBy(p => p.Key.BitCount).OrderBy(r => r.Key).ToList();
+
+foreach (var occurance in occurances)
+{
+    int max = occurance.Max(r => r.Value.Count);
+    Console.WriteLine($"Occurance of {occurance.Key} bits is {occurance.Count()} times with {max} as the highest occurance.");
+    foreach (var item in occurance.Where(r => r.Value.Count == max))
+    {
+        Console.WriteLine($"  Pattern: {item.Key.Draw} has {item.Value.Count} items");
+        if (max <= 2)
+        {
+            Console.WriteLine($"    We're not showing the draws that have only 2 occurances");
+        }
+        else
+        {
+            foreach (var group in item.Value)
+            {
+                Console.WriteLine($"    Draw: {group.Draw}");
+            }
+        }
+    }
+}
